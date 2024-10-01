189f7c8abbb55dc2656d95aeab6cca13aae4190d
FelixH3r <felix.hermanutz@gmx.at>
2024-10-01 06:04:17 +0200

    Update AdressForm.vue

--- components/cart/AdressForm.vue
+++ components/cart/AdressForm.vue
@@ -32,7 +32,7 @@
              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
              :class="{'border-red-500': store.isFieldInvalid.street}"
              v-model="store.formData.street"
-             placeholder="name@flowbite.com" required/>
+             placeholder="Straße mit Hausnummer" required/>
     </div>
     <div class="mb-5">
       <label for="city" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stadt:</label>
