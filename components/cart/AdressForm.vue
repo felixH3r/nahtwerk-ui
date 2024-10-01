9b4129b4a721efc870a409be2ae98582edd63192
FelixH3r <felix.hermanutz@gmx.at>
2024-10-01 06:08:24 +0200

    Update AdressForm.vue

--- components/cart/AdressForm.vue
+++ components/cart/AdressForm.vue
@@ -40,7 +40,7 @@
              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
              :class="{'border-red-500': store.isFieldInvalid.city}"
              v-model="store.formData.city"
-             required/>
+             placeholder="Stadt" required/>
     </div>
     <div class="mb-5">
       <label for="postalCode" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postleitzahl:</label>
@@ -48,7 +48,7 @@
              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
              :class="{'border-red-500': store.isFieldInvalid.postalCode}"
              v-model="store.formData.postalCode"
-             required/>
+             placeholder="Postleitzahl" required/>
     </div>
     <div class="flex items-start mb-5">
       <div class="flex items-center h-5">
