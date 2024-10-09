1ee09ba6ffd0e74f0c36d0df46f59cf74fcff1c6
FelixH3r <felix.hermanutz@gmx.at>
2024-10-09 21:14:13 +0200

    edit feature section

--- components/home/FeatureSection.vue
+++ components/home/FeatureSection.vue
@@ -1,55 +1,70 @@
 <template>
   <section class="dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-      <div class="max-w-screen-md mb-8 lg:mb-16">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business
-          teams like yours</h2>
-        <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology,
-          innovation, and capital can unlock long-term value and drive economic growth.</p>
-      </div>
+      <!--      <div class="max-w-screen-md mb-8 lg:mb-16">-->
+      <!--        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business-->
+      <!--          teams like yours</h2>-->
+      <!--        <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology,-->
+      <!--          innovation, and capital can unlock long-term value and drive economic growth.</p>-->
+      <!--      </div>-->
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
         <div>
           <div
               class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
-                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                    clip-rule="evenodd"></path>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4.12695 13.218L12.026 21L20.2707 12.8774" stroke="#231F20" stroke-width="2"
+                    stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" class="my-path"></path>
+              <path
+                  d="M12.0039 4.68819C14.2887 2.43727 18.0014 2.43727 20.2862 4.68819C22.571 6.94652 22.571 10.5969 20.2937 12.8552"
+                  stroke="#231F20" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
+                  stroke-linejoin="round" class="my-path"></path>
+              <path
+                  d="M11.9959 4.68819C9.71112 2.43727 5.99836 2.43727 3.71358 4.68819C1.42881 6.93912 1.42881 10.5969 3.71358 12.8478M3.71358 12.8478L3.73297 12.8668M3.71358 12.8478L4.11943 13.2106"
+                  stroke="#231F20" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round"
+                  stroke-linejoin="round" class="my-path"></path>
             </svg>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
-          <p class="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate seamlessly with all the
-            organization and hit your marketing goals every month with our marketing plan.</p>
+          <h3 class="mb-2 text-xl font-bold dark:text-white">Mit Liebe gemacht</h3>
+          <p class="text-gray-500 dark:text-gray-400">Babykleidung, liebevoll von Hand gefertigt. Jeder Stich und jedes
+            Detail strahlt individualität aus.</p>
         </div>
         <div>
           <div
               class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
-                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
-                  d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
+                  d="M17 21L20.3282 16.0077C20.6616 15.5075 20.8284 15.2574 20.9142 14.974C21 14.6906 21 14.39 21 13.7889V9.5C21 8.67157 20.3284 8 19.5 8C18.6716 8 18 8.67157 18 9.5V12.6667C18 13.3275 18 13.6579 17.8974 13.9658C17.7947 14.2737 17.5965 14.538 17.2 15.0667L16.5 16"
+                  stroke="black" stroke-width="2" stroke-linecap="round" class="my-path"></path>
+              <path
+                  d="M7 21L3.6718 16.0077C3.33835 15.5075 3.17163 15.2574 3.08582 14.974C3 14.6906 3 14.39 3 13.7889V9.5C3 8.67157 3.67157 8 4.5 8C5.32843 8 6 8.67157 6 9.5V12.6667C6 13.3275 6 13.6579 6.10263 13.9658C6.20527 14.2737 6.40351 14.538 6.8 15.0667L7.5 16"
+                  stroke="black" stroke-width="2" stroke-linecap="round" class="my-path"></path>
+              <path
+                  d="M14 21.0001V17.1232C14 15.7035 14 14.9937 14.2986 14.5104C14.5496 14.1041 14.9377 13.8011 15.3928 13.6561C15.934 13.4836 16.6227 13.6558 18 14.0001"
+                  stroke="black" stroke-width="2" stroke-linecap="round" class="my-path"></path>
+              <path
+                  d="M10 21.0001V17.1232C10 15.7035 10 14.9937 9.70142 14.5104C9.45039 14.1041 9.06226 13.8011 8.60722 13.6561C8.066 13.4836 7.37733 13.6558 6 14.0001"
+                  stroke="black" stroke-width="2" stroke-linecap="round" class="my-path"></path>
+              <path
+                  d="M8.34887 7.44953C8.40233 7.50558 8.45764 7.55851 8.51459 7.60834L10.5486 9.74053C11.2279 10.4527 11.5676 10.8088 11.9957 10.8088C12.4238 10.8088 12.7635 10.4527 13.4429 9.74053L15.2859 7.80854C15.32 7.77278 15.337 7.75489 15.3525 7.73999C15.368 7.72509 15.4107 7.68742 15.4961 7.61207C15.5501 7.56441 15.6026 7.51393 15.6535 7.46064C16.6216 6.44581 16.6155 4.79413 15.64 3.77149C14.6645 2.74886 13.0889 2.74253 12.1208 3.75736C12.0534 3.82799 11.937 3.82946 11.8696 3.75883C10.9015 2.744 9.32858 2.74752 8.35636 3.76668C7.38414 4.78584 7.38079 6.43471 8.34887 7.44953Z"
+                  stroke="black" stroke-width="2" class="my-path"></path>
             </svg>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Legal</h3>
-          <p class="text-gray-500 dark:text-gray-400">Protect your organization, devices and stay compliant with our
-            structured workflows and custom permissions made for you.</p>
+          <h3 class="mb-2 text-xl font-bold dark:text-white">Regional & Nachhaltig</h3>
+          <p class="text-gray-500 dark:text-gray-400">Babykleidung, die auf Regionalität und Nachhaltigkeit setzt. Meine
+            Stoffe beziehe ich von regionalen Händlern.</p>
         </div>
         <div>
           <div
               class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor"
-                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                    d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-                    clip-rule="evenodd"></path>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
-                  d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
+                  d="M22.9983 19.3008C22.9983 15.6557 20.0433 12.7007 16.3982 12.7007M16.3982 12.7007C12.753 12.7007 9.79803 15.6557 9.79803 19.3008M16.3982 12.7007C14.2718 12.7007 12.5481 10.9764 12.5481 8.85007C12.5481 6.72374 14.2719 5 16.3982 5C18.5245 5 20.2483 6.72374 20.2483 8.85007C20.2483 10.9764 18.5245 12.7007 16.3982 12.7007ZM1 18.2008C1 15.1632 3.46248 12.7007 6.50011 12.7007C8.0387 12.7007 9.42974 13.3324 10.428 14.3507M3.74818 9.87261C3.74818 11.3914 4.97942 12.6227 6.49823 12.6227C8.01704 12.6227 9.24828 11.3914 9.24828 9.87261C9.24828 8.3538 8.01704 7.12256 6.49823 7.12256C4.97942 7.12256 3.74818 8.3538 3.74818 9.87261Z"
+                  stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="my-path"></path>
             </svg>
           </div>
-          <h3 class="mb-2 text-xl font-bold dark:text-white">Business Automation</h3>
-          <p class="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and much more. Now power
-            up with hundreds of new templates to help you get started.</p>
+          <h3 class="mb-2 text-xl font-bold dark:text-white">Entwicklungsgerecht</h3>
+          <p class="text-gray-500 dark:text-gray-400">Als Physiotherapeutin achte ich auf entwicklungsgerechte und
+            unterstützende Schnitte.</p>
         </div>
       </div>
     </div>
--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -9,15 +9,15 @@
         <p class="max-w-2xl mb-6 font-extralight text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Tauche
           ein
           in die Welt von Nahtwerk und lasse dich begeistern.</p>
-        <a href="#"
-           class="inline-flex items-center justify-center w-64 px-5 py-3 mr-3 text-base font-light text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+        <NuxtLink to="/shop"
+                  class="inline-flex items-center justify-center w-64 px-5 py-3 mr-3 text-base font-light text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
           Schau's dir jetzt an
           <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd"
                   d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                   clip-rule="evenodd"></path>
           </svg>
-        </a>
+        </NuxtLink>
       </div>
 
       <div class="md:h-3/4 md:mt-0 md:col-span-5 md:flex rounded-full shadow-2xl">
