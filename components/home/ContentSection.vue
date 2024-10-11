f5a08da8fe513630810bbec6fb86feb75e8f17c5
FelixH3r <felix.hermanutz@gmx.at>
2024-10-11 19:37:13 +0200

    home page content

--- components/home/ContentSection.vue
+++ components/home/ContentSection.vue
@@ -1,14 +1,23 @@
 <template>
-  <section class=" dark:bg-gray-900">
+  <section class=" dark:bg-gray-900 mb-10">
     <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
       <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the
-          wheel</h2>
-        <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be
-          simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple
-          and quick, but big enough to deliver the scope you want at the pace you need.</p>
-        <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and
-          quick.</p>
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Wir haben das Rad nicht
+          neu erfunden – aber jedes Stück mit Liebe gemacht.</h2>
+        <p class="mb-4">Bei Nahtwerk geht es nicht um Massenproduktion, sondern um handgemachte Babykleidung, die mit
+          viel Herz und Liebe zum Detail entsteht. Jedes meiner Stücke ist sorgfältig gefertigt, um den besonderen
+          Bedürfnissen deines Babys gerecht zu werden.
+
+          Ich lege großen Wert auf Qualität und Nachhaltigkeit. Alle Materialien sind weich, hautfreundlich und
+          perfekt für die empfindliche Babyhaut geeignet. Klein, aber fein – bei Nahtwerk steht die Individualität und
+          das
+          Wohlbefinden deines Babys im Vordergrund.
+
+          Mit Nahtwerk entscheidest du dich für langlebige Babykleidung, die nicht nur gut aussieht, sondern auch
+          praktisch und komfortabel ist.
+        </p>
+        <!--        <p>We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and-->
+        <!--          quick.</p>-->
       </div>
       <div class="grid grid-cols-2 gap-4 mt-8">
         <img class="w-full rounded-lg"
--- components/home/FeatureSection.vue
+++ components/home/FeatureSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="dark:bg-gray-900">
+  <section class="dark:bg-gray-900 mb-10">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <!--      <div class="max-w-screen-md mb-8 lg:mb-16">-->
       <!--        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business-->
