f9117b42ac2d73ccc6a45449ad0e55ed0127f7cb
FelixH3r <felix.hermanutz@gmx.at>
2024-12-06 08:29:35 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nahtwerk - Mit Liebe gemacht!',
+      htmlAttrs: {
+        lang: 'de'
+      },
+      meta: [
+        {charset: 'utf-8'},
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Babykleidung, die auf Regionalität und Nachhaltigkeit setzt. Stoffe von regionalen Händlern, liebevoll von Hand verarbeitet.'
+        },
+        {name: 'format-detection', content: 'telephone=no'},
+      ],
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: {enabled: true},
   modules: ['nuxt-medusa', '@nuxtjs/tailwindcss', '@pinia/nuxt', "@nuxt/image", '@nuxtjs/seo'],
