508b6664f886ac5c3a17f8a4253cf057eef54408
FelixH3r <felix.hermanutz@gmx.at>
2025-01-26 21:47:30 +0100

    add vercel analytics

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
 
 <script setup lang="ts">
   import {useFlowbite} from "~/composables/useFlowbite";
+  import {Analytics} from '@vercel/analytics/nuxt';
 
   onMounted(async () => {
     useFlowbite((flowbite: any) => flowbite.initFlowbite());
