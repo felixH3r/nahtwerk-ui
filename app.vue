5d5392d903bfaa9ed3af9130ff339a5a27b99ffe
FelixH3r <felix.hermanutz@gmx.at>
2025-01-26 22:40:14 +0100

    remove analytics

--- app.vue
+++ app.vue
@@ -4,7 +4,6 @@
 
 <script setup lang="ts">
   import {useFlowbite} from "~/composables/useFlowbite";
-  import {Analytics} from '@vercel/analytics/nuxt';
 
   onMounted(async () => {
     useFlowbite((flowbite: any) => flowbite.initFlowbite());
